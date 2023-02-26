2cb8abaccf04273d5f9bc9d74556c90e42ff1c88
elenaanisimova <elenanisimovaa@gmail.com>
2023-02-26 17:06:57 +0100

    создание теста Добавление группы

--- test_add_group.py
+++ test_add_group.py
@@ -7,38 +7,36 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class TestAddGroup(unittest.TestCase):
+class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome(executable_path=r'')
+        self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
     
-    def test_add_group(self):
+    def test_untitled_test_case(self):
         wd = self.wd
-        wd.get("https://software-testing.ru/lms/mod/page/view.php?id=310370")
-        wd.find_element_by_id("id_videojs_63ed29ab0f91d_1_html5_api").click()
+        wd.get("http://localhost/addressbook/group.php")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        wd.get("http://localhost/addressbook/group.php?new=New+group")
+        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("qwerty")
+        wd.find_element_by_name("group_name").send_keys("qwertyu")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("qwerty")
+        wd.find_element_by_name("group_header").send_keys("qwertyu")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("qwerty")
+        wd.find_element_by_name("group_footer").send_keys("qwertyu")
         wd.find_element_by_name("submit").click()
-        wd.get("http://localhost/addressbook/group.php")
         wd.find_element_by_link_text("group page").click()
         wd.find_element_by_link_text("Logout").click()
-        #ERROR: Caught exception [unknown command []]
-        #ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
     
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
@@ -49,10 +47,10 @@ class TestAddGroup(unittest.TestCase):
         try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
-
-
+    
     def tearDown(self):
         self.wd.quit()
+        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
