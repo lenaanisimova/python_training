edc472f73d4cd5f9187ab7d6646999b9b1077436
elenaanisimova <elenanisimovaa@gmail.com>
2023-04-14 12:34:29 +0200

    Улучшение отчета по тесту add_group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from model.group import Group
+import pytest
 
-def test_add_group(app):
+
+testdata = [Group(name="qwertyu", header="qwertyu", footer="qwertyu"),
+                Group(name="", header="", footer="")
+    ]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
+    pass
     old_groups = app.group.get_group_list()
-    group = Group(name="qwertyu", header="qwertyu", footer="qwertyu")
     app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
