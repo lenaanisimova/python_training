4b8e16b5c1154ee619691d2aea4b4599b7e1a3f1
elenaanisimova <elenanisimovaa@gmail.com>
2023-04-27 12:22:22 +0200

    Задание 16.Реализация опции,к-рая указывает browser и beseUrl

--- fixture/application.py
+++ fixture/application.py
@@ -4,12 +4,21 @@ from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie("Unrecognized browser %s" % browser)
+        else:
+            raise ValueError
+
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def open_group_page(self):
         # open group page
@@ -26,7 +35,7 @@ class Application:
     def open_home_page(self):
         # open home page
         wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
+        wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
